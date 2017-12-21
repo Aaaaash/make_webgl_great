610c3e5760ec28614e7fb7c9287be7c402d4336d
sakuraash <841105127@qq.com>
2017-12-21 12:06:27 +0800

    second commit

---
+++ 01.Getting started with WebGL/index.js
@@ -0,0 +1,30 @@
+const canvas = document.querySelector('#canvas');
+const gl = initialWebGL(canvas);
+
+if (gl) {
+  canvas.width = 852;
+  canvas.height = 480;
+  gl.viewport(0, 0, canvas.width, canvas.height);
+
+  // 设置清除颜色为黑色，不透明
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);    
+  // 开启“深度测试”, Z-缓存
+  gl.enable(gl.DEPTH_TEST); 
+  // 设置深度测试，近的物体遮挡远的物体
+  gl.depthFunc(gl.LEQUAL); 
+  // 清除颜色和深度缓存
+  gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);     
+}
+
+function initialWebGL(element) {
+  let webgl;
+  try {
+    webgl = element.getContext("webgl") || element.getContext("experimental-webgl");
+  }
+  catch(e) {}
+  if (!webgl) {
+    alert("WebGL初始化失败，可能是因为您的浏览器不支持。");
+    webgl = null;
+  }
+  return webgl;
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-## Getting_started_with_WebGL
+## WebGL tutorial
